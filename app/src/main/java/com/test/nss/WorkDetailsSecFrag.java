52a6a04fa54102210058c37267a7164053b1d6a8
Your Name <you@example.com>
2020-07-11 20:14:00 +0530

    feilds added into sechalf of workhours

--- app/src/main/java/com/test/nss/WorkDetailsSecFrag.java
+++ app/src/main/java/com/test/nss/WorkDetailsSecFrag.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,8 @@ import androidx.fragment.app.FragmentManager;
 
 
 public class WorkDetailsSecFrag extends Fragment {
+    View root;
+    ListView compHours;
 
     FragmentManager fm;
 
@@ -19,18 +22,22 @@ public class WorkDetailsSecFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        fm = requireActivity().getSupportFragmentManager();
-        return inflater.inflate(R.layout.fragment_work_details_sec, container, false);
+//        fm = requireActivity().getSupportFragmentManager();
+//        return inflater.inflate(R.layout.fragment_work_details_sec, container, false);
+        root = inflater.inflate(R.layout.fragment_work_details_first, container, false);
+        return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        compHours = root.findViewById(R.id.comp_hours);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+        fm = requireActivity().getSupportFragmentManager();
         fm.popBackStack("WorkFrag", 0);
     }
 }
